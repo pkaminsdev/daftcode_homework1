056b23c39d646570b3b85fce490343200aab11de
Paweł <k.pawel.kaminski@gmail.com>
2019-03-23 16:45:44 +0100

    Webpack app - homework

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development',
+    plugins: [new HtmlWebpackPlugin()],
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
