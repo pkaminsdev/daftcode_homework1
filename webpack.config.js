3a4357bb72cf115dddfe71473c038bc362c347af
Paweł <k.pawel.kaminski@gmail.com>
2019-04-01 23:34:52 +0200

    Homework_01.04

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -18,7 +19,10 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: "[name].css",
             chunkFilename: "[id].css"
-        })
+        }),
+        new CopyWebpackPlugin([
+            { from: 'src/assets', to: 'assets'}
+        ])
     ],
     module: {
         rules: [
